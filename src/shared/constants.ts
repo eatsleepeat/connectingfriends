3887e28b1d66f354e4eb1d97a9ec2efa6df9cca3
eatsleepeat <60551987+eatsleepeat@users.noreply.github.com>
2021-02-20 14:52:07 -0800

    Delete src directory

--- src/shared/constants.ts
+++
@@ -1,5 +0,0 @@
-export const topics = [
-    "sports", "gaming", "literature", "comedy", "history", "anime", "K-Pop", "school","superheroes", "Harry Potter", "fanatasy", "science", "math", "action movies", "comic books", "cartoons", "computers", "sci-fi", "adventure films", "Star Wars", "writing"
-];
-
-export const API_URL:string = "";
